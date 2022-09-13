de166c6cd0a89938fbd39cd6262d7801a42cf70b
Bruno <bruno.pacheco@tusta.com.br>
2022-09-13 20:25:20 -0300

    feat: remove unnecessary examples

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,9 @@
-
-interface ButtonProps {
-  title: string;
-}
-
-function Button(props: ButtonProps) {
-  return (
-    <button>{props.title}</button>
-  )
-}
+import './styles.css'
 
 function App() {
   return (
     <div>
-      <Button title="Send 1" />
-      <Button title="Send 2" />
-      <Button title="Send 3" />
-      <Button title="Hello world" />
+
     </div>
   )
 }
