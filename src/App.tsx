b59e0a009df77a8b130fdfc2100e7a4611ea6be3
Bruno <bruno.pacheco@tusta.com.br>
2022-10-17 20:15:33 -0300

    feat: create example to introduce react explanation

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
 import './styles/main.css'
+import { useState } from 'react'
 import logoImg from './assets/logo-nlw-esports.png'
 import { GameBanner } from './components/GameBanner/GameBanner'
 import { CreateAdBanner } from './components/CreateAdBanner/CreateAdBanner'
 
 function App() {
+
+  const [hasUserClickedOnButton, setHasUserClickedOnButton] = useState(false)
+
+  function handleButtonClick()
+  {
+    setHasUserClickedOnButton(true)
+  }
+
   return (
     <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
       <img src={logoImg} alt="" />
+
       <h1 className="text-6xl text-white font-black mt-20">
         Seu <span className="text-transparent bg-nlw-gradient bg-clip-text">duo</span>  está aqui.
       </h1>
+
+      <button onClick={handleButtonClick}>Clique aqui</button>
+
+      {hasUserClickedOnButton ?"Usuário clicou": null }
+      
+
       <div className="grid grid-cols-6 gap-6 mt-16">
         
         <GameBanner bannerUrl="/game-1.png" title="league of Legends" adsCount={5} />
@@ -17,7 +33,6 @@ function App() {
       </div>
 
       <CreateAdBanner />
-
 
     </div>
   )
